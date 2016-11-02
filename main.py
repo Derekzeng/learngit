0a3150ec8399cde0f162ed06d7be6af9e46e7298
derek <910153304@qq.com>
2016-11-02 10:55:16 +0800

    commit to your email

---
+++ main.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+import tornado
+import tornado.web
+
+
+class GenericHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("rime/hello.html")
+
+import os
+
+TEMPLATE_DIR=os.path.join(os.path.dirname(__file__),'templates')
+setting = dict(debug=True,template_path=TEMPLATE_DIR)
+
