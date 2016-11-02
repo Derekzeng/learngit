17a4cfcc7f6b41016e3dafbaa1f220e823f5a666
derek <910153304@qq.com>
2016-11-02 11:04:22 +0800

    sss

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tornado
 import tornado.web
 
 
+
 class GenericHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("rime/hello.html")
@@ -11,4 +12,7 @@ import os
 
 TEMPLATE_DIR=os.path.join(os.path.dirname(__file__),'templates')
 setting = dict(debug=True,template_path=TEMPLATE_DIR)
+
+
+application = tornado.web.Application
 
