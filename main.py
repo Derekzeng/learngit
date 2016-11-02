ad49ce6170692224d4c00d14d2b1a0d399935765
derek <910153304@qq.com>
2016-11-02 16:19:35 +0800

    this is new

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-#coding=utf-8
-import tornado
-import tornado.web
-
-
-
-class GenericHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("rime/hello.html")
-
-import os
-
-TEMPLATE_DIR=os.path.join(os.path.dirname(__file__),'templates')
-setting = dict(debug=True,template_path=TEMPLATE_DIR)
-
-
-application = tornado.web.Application
-
-# kiss something
-=======
 #!/usr/bin/ev python
 #coding=utf-8
 
@@ -55,5 +34,4 @@ if __name__ == "__main__":
 
     tornado.ioloop.IOLoop.instance().start()
 
-
->>>>>>> 9f11dcdc329da99c27366e914410c53e04fa521c
+    #this  is new 
